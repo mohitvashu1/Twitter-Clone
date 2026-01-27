c0749c30f5dfd8b78f13847b506e9681f88ec63f
mohitvashu1 <mohitchoubeybca@gmail.com>
2026-01-28 02:13:44 +0530

    Select Images

--- components/CreatePost/index.tsx
+++ components/CreatePost/index.tsx
@@ -1,11 +1,20 @@
 import { useCurrentUser } from "@/hooks/user";
 import Image from "next/image";
+import { useCallback } from "react";
+import { CiImageOn } from "react-icons/ci";
 
 
 
 
 const CreatePost: React.FC = () => {
   const {user}=useCurrentUser();
+  const haldleSelectImage = useCallback(()=>{
+    const input =document.createElement('input')
+    input.setAttribute('type','file')
+    input.setAttribute('accept','image/*')
+    input.click()
+    
+  },[])
   return (
     <>
     <div className="border border-r-0 border-l-0 border-b-0 border-gray-600 p-5 ">
@@ -18,7 +27,15 @@ const CreatePost: React.FC = () => {
                />}
         </div>
         <div className="col col-span-11">
-            <textarea className=" w-full bg-transparent text-xl px-3 border-b border-slate-700" rows={3} placeholder="What's Happening"></textarea>
+            <textarea className=" w-full bg-transparent text-xl px-3 border-b border-slate-700" 
+            rows={2} placeholder="What's Happening"></textarea>
+            <div className="mt-2 flex justify-between items-center">
+              <CiImageOn onClick={haldleSelectImage}
+              className="text-2xl hover:text-[#1d9bf0]"/>
+              <button className="  bg-[#f5eaea] hover:bg-[#949393] text-black font-semibold text-lg py-1 rounded-full w-25">
+            Post
+          </button>
+            </div>
         </div>
        
       </div>

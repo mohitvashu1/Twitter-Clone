d8d4957f9f099003aa2efabbb4b7a30a0851de53
mohitvashu1 <mohitchoubeybca@gmail.com>
2026-02-10 22:57:14 +0530

    id

---
+++ pages/[id].tsx
@@ -0,0 +1,34 @@
+import Header from "@/components/Header";
+import Sidebar from "@/components/LeftCard";
+import RightCard from "@/components/RightCard";
+import { useCurrentUser } from "@/hooks/user";
+import { NextPage } from "next";
+
+const UserProfilePage : NextPage=()=>{
+    const { user } = useCurrentUser();
+    return(
+        <div>
+            <Header/>
+            <div>
+                <div className="grid grid-cols-12 h-screen overflow-hidden">
+                    {/* SIDEBAR(LEFT) */}
+                     <div className="col-span-2 sm:col-span-3 h-screen sticky top-0 overflow-hidden ">
+                         <Sidebar/>
+                       </div>
+
+                    {/* Profile */}
+                         <div className="col-span-10 sm:col-span-6 border-x border-gray-600 h-screen min-h-0 overflow-y-auto ">
+
+                         </div>
+                       
+                     {/* Right Section */}
+                         <div className="hidden sm:block col-span-3 h-screen sticky top-0 overflow-hidden">
+                         {user && <RightCard user={user} />}
+                        </div>
+                </div>
+            </div>
+        </div>
+
+    )
+}
+export default UserProfilePage; 
